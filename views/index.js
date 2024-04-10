3f2f4ae58a311c1206d46fd9a71c7c9580ae7411
Adonis <adonis.casado@outlook.com>
2024-04-10 11:17:10 -0400

    Update files and remove obsolete directory

--- views/index.js
+++ views/index.js
@@ -1,15 +1,16 @@
 const config = {
-  cUrl: 'https://api.countrystatecity.in/v1/countries'
+  cUrl: 'https://www.universal-tutorial.com/api/countries/'
+  
 }
 
-const toggleAddLocation = () => {
-  const addLocationSection = document.getElementById('addLocationSection');
-  if (isNewTrip) {
-    addLocationSection.style.display = 'block';
-  } else {
-    addLocationSection.style.display = 'none';
-  }
-};
+// const toggleAddLocation = () => {
+//   const addLocationSection = document.getElementById('addLocationSection');
+//   if (isNewTrip) {
+//     addLocationSection.style.display = 'block';
+//   } else {
+//     addLocationSection.style.display = 'none';
+//   }
+// };
 
-toggleAddLocation();
+// toggleAddLocation();
 
