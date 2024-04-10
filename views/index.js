c260925f8fc27d12194706347afb11c50017c5ce
Adonis <adonis.casado@outlook.com>
2024-04-10 00:54:18 -0400

    add front-end

--- views/index.js
+++ views/index.js
@@ -1,3 +1,15 @@
-let config = {
+const config = {
   cUrl: 'https://api.countrystatecity.in/v1/countries'
 }
+
+const toggleAddLocation = () => {
+  const addLocationSection = document.getElementById('addLocationSection');
+  if (isNewTrip) {
+    addLocationSection.style.display = 'block';
+  } else {
+    addLocationSection.style.display = 'none';
+  }
+};
+
+toggleAddLocation();
+
